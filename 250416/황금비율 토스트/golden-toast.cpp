8c98cfb2db328b4f8e25a46ee11e9b337ce8fe5e
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-16 01:41:01 +0900

    Title: 황금비율 토스트, Time: 17ms, Memory: 1MB, Status: Runtime Error - Codetree

---
+++ 250416/황금비율 토스트/golden-toast.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int n, m;
+
+int main() {
+    list<char> bread;
+    list<char>::iterator it;
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++){
+        char bread_1;
+        cin >> bread_1;
+        bread.push_back(bread_1);
+    }
+    it = bread.end();
+    for (int i = 0; i < m; i++) {
+        char c;
+        char command;
+        cin >> command;
+        if (command == 'P') {
+            cin >> c;
+        }
+        if(command == 'L'){
+            if(it == bread.begin()) continue;
+            it--;
+        }
+        else if(command == 'R'){
+            if(it == bread.end()) continue;
+            it++;
+        }
+        else if(command == 'D'){
+            if(it == bread.end()) continue;
+            bread.erase(it);
+        }
+        else if(command == 'P'){
+            bread.insert(it,c);
+        }
+    }
+    
+    for(it = bread.begin(); it != bread.end(); it++){
+        cout << *it;
+    }
+
+    
+
+    return 0;
+}
