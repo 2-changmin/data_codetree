af801a2c2aa6e2a6e714578d6e9b280806d44f4a
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-20 22:14:03 +0900

    Title: 재귀함수의 꽃, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250420/재귀함수의 꽃/the-flower-of-the-recursive-function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print1(int N){
+    if(N == 0) return ;
+    
+    cout << N << " ";
+    print1(N-1);
+    cout << N << " ";
+}
+
+int main() {
+    cin >> N;
+    print1(N);
+    return 0;
+}
