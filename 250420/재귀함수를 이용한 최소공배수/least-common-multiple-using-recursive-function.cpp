fa8e793a2cb211ffd66790bdbf2841cc4bab62fa
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-20 22:47:16 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250420/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[10];
+
+int gcd(int a,int b){
+    if(b == 0) return a;
+    return gcd(b,a%b);
+}
+
+int lcm(int a,int b){
+    return (a*b)/gcd(a,b);
+}
+
+int gcc(int a,int b){
+    if(a==n) return b;
+    return gcc(a+1,lcm(arr[a],b));
+}
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int result = gcc(1,arr[0]);
+    cout << result;
+
+    return 0;
+}
