ed670b0ec9f911552383385749a9d9d307180010
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-21 21:09:49 +0900

    Title: 퀵 정렬 구현, Time: 34ms, Memory: 1MB, Status: Passed - Codetree

--- 250421/퀵 정렬 구현/implement-quick-sort.cpp
+++ 250421/퀵 정렬 구현/implement-quick-sort.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int n;
 int arr[100000];
 
-int partition(int low,int high){
+int partition(int low, int high){
     int pivot = arr[high];
-    int i = low - 1;
-
-    for(int j = low; j < high; j++){
+    int i = low - 1; // 파란색 화살표가 가리키는 곳
+    for(int j = low; j < high; j++){ // j <= high 아닌 이유는 high 값은 피벗이기때문
         if(arr[j] < pivot){
             i++;
             swap(arr[i],arr[j]);
@@ -19,8 +19,8 @@ int partition(int low,int high){
     return i+1;
 }
 
-void quick_sort(int low,int high){
-    if(low<high){
+void quick_sort(int low, int high){
+    if(low < high){
         int pos = partition(low,high);
         quick_sort(low,pos-1);
         quick_sort(pos+1,high);
