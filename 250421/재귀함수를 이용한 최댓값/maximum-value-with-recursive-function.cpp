f09dc415c652f8483c18105fbdc02fcae390272d
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-21 18:16:29 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250421/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
+++ 250421/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 int n;
 int arr[100];
 int max2 = 0;
-int max1(int n){
-    if(n == 1) return arr[0];
+int max1(int n){ 
+    if(n == 0) return arr[0]; // n이 0 일때 arr[0] 을 반환
     
     return max(max1(n - 1), arr[n]);
 }
@@ -20,7 +20,7 @@ int main() {
         cin >> arr[i];
     }
 
-    cout << max1(n);
+    cout << max1(n-1);
 
     return 0;
 }
