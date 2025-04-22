83305e18f6a8e8b7d669940553db7fa703ae991d
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-22 15:50:57 +0900

    Title: 병합 정렬 구현, Time: 39ms, Memory: 1MB, Status: Passed - Codetree

--- 250422/병합 정렬 구현/implement-merge-sort.cpp
+++ 250422/병합 정렬 구현/implement-merge-sort.cpp
@@ -4,11 +4,12 @@ using namespace std;
 
 int n;
 int arr[100000];
-int merged_arr[100000];
+int merge_arr[100000];
+
 void merge(int,int,int);
-void merge_sort(int low, int high){
+void merge_sort(int low,int high){
     if(low < high){
-        int mid = (low + high) / 2;
+        int mid = (low+high) / 2;
         merge_sort(low,mid);
         merge_sort(mid+1,high);
         merge(low,mid,high);
@@ -21,29 +22,30 @@ void merge(int low,int mid,int high){
     int j = mid + 1;
     while(i <= mid && j <= high){
         if(arr[i] < arr[j]){
-            merged_arr[k] = arr[i];
+            merge_arr[k] = arr[i];
             k++;
             i++;
         }
         else{
-            merged_arr[k] = arr[j];
+            merge_arr[k] = arr[j];
             k++;
             j++;
         }
     }
     while(i <= mid){
-        merged_arr[k] = arr[i];
+        merge_arr[k] = arr[i];
         k++;
         i++;
     }
     while(j <= high){
-        merged_arr[k] = arr[j];
+        merge_arr[k] = arr[j];
         k++;
         j++;
     }
     for(int i = low; i <= high; i++){
-        arr[i] = merged_arr[i];
+        arr[i] = merge_arr[i];
     }
+    
 }
 
 int main() {
@@ -53,7 +55,7 @@ int main() {
         cin >> arr[i];
     }
 
-    merge_sort(0,n-1);
+    merge_sort(0, n-1);
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
