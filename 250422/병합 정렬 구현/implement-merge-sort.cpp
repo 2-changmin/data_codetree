676fbbbf40565b2d5601996bfdb15e0dd5598f3f
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-04-22 12:20:06 +0900

    Title: 병합 정렬 구현, Time: 37ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250422/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+int merged_arr[100000];
+void merge(int,int,int);
+void merge_sort(int low, int high){
+    if(low < high){
+        int mid = (low + high) / 2;
+        merge_sort(low,mid);
+        merge_sort(mid+1,high);
+        merge(low,mid,high);
+    }
+}
+
+void merge(int low,int mid,int high){
+    int i = low;
+    int k = low;
+    int j = mid + 1;
+    while(i <= mid && j <= high){
+        if(arr[i] < arr[j]){
+            merged_arr[k] = arr[i];
+            k++;
+            i++;
+        }
+        else{
+            merged_arr[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while(i <= mid){
+        merged_arr[k] = arr[i];
+        k++;
+        i++;
+    }
+    while(j <= high){
+        merged_arr[k] = arr[j];
+        k++;
+        j++;
+    }
+    for(int i = low; i <= high; i++){
+        arr[i] = merged_arr[i];
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    merge_sort(0,n-1);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
