7eb7a90c0dddd41274f2026d25343ff0905567aa
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-05-14 00:44:43 +0900

    Title: 수열 조작, Time: 8ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250514/수열 조작/sequence-manipulation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+    deque<int> s;
+    for(int i = 1; i <= N; i++){
+        s.push_back(i);
+    }
+    while(s.size() != 1){
+        s.pop_front();
+        int a = s.front();
+        s.pop_front();
+        s.push_back(a);
+    }
+    cout << s.front();
+    return 0;
+}
