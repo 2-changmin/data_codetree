abf77856de9624b2519d9f673da5ef112f8f8c4d
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-06-12 14:48:51 +0900

    Title: 정수 명령 처리 2, Time: 17ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250612/정수 명령 처리 2/process-numeric-commands-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+int N;
+string command[10000];
+int A;
+
+int main() {
+    cin >> N;
+    queue<int> s;
+    for (int i = 0; i < N; i++) {
+        cin >> command[i];
+        if (command[i] == "push") {
+            cin >> A;
+            s.push(A);
+        }
+        else if(command[i] == "pop"){
+            cout << s.front() << endl;
+            s.pop();
+        }
+        else if(command[i] == "size"){
+            cout << s.size() << endl;
+        }
+        else if(command[i] == "empty"){
+            cout << s.empty() << endl;
+        }
+        else if(command[i] == "front"){
+            cout << s.front() << endl;
+        }
+    }
+
+    
+
+    return 0;
+}
