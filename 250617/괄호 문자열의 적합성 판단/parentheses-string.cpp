c0f109e502dfbc0d02b35ae873d7f55edd176342
leechangmin <101034582+2-changmin@users.noreply.github.com>
2025-06-17 13:38:59 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250617/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+string str;
+
+int main() {
+    cin >> str;
+    stack<char> s;
+
+    for(int i:str){
+        if(i == '('){
+            s.push('(');
+        }
+        else if(i == ')'){
+            if(s.empty()){
+                cout << "No" << endl;
+                return 0;
+            }
+            s.pop();
+        }
+    }
+    if(s.empty()) cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
