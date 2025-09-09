ee57a164784758351b90f8f3d612c8e620aa550e
Lee Chang Min <101034582+2-changmin@users.noreply.github.com>
2025-09-09 17:29:43 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250909/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int chu = 13;
+        double bi = 0.165;
+        double re = chu * bi;
+        System.out.printf("%d * %.6f = %.6f",chu,bi,re);
+    }
+}
