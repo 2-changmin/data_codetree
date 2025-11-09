8784e4e5e026d2f9325429535c1c82729c1d8765
Lee Chang Min <101034582+2-changmin@users.noreply.github.com>
2025-11-09 14:22:18 +0900

    Title: 시력 검사, Time: 139ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 251109/시력 검사/eye-test.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextFloat();
+        double b = sc.nextFloat();
+        if(a>=1.0&&b>=1.0) System.out.print("High");
+        else if(a>=0.5&&b>=0.5) System.out.print("Middle");
+        else System.out.print("Low");
+    }
+}
