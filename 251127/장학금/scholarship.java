168808eab83581730ff3ca1fcd1690639f7aaae2
Lee Chang Min <101034582+2-changmin@users.noreply.github.com>
2025-11-27 00:44:58 +0900

    Title: 장학금, Time: 127ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251127/장학금/scholarship.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>=90 && b>=95) System.out.print("100000");
+        else if(a>=90 && b>=90) System.out.print("50000");
+        else System.out.print("0");
+    }
+}
