ab527395503bb00ea701bd8adb145d79ef8281a4
Lee Chang Min <101034582+2-changmin@users.noreply.github.com>
2025-11-28 23:55:46 +0900

    Title: 좀 더 어려운 수학 점수, Time: 125ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251128/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        if(a>c) System.out.print("A");
+        else if(a==c && b > d) System.out.print("A");
+        else System.out.print("B");
+        
+        
+    }
+}
