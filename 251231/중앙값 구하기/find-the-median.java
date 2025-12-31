1a03ea6b96499ece3343a2ee6fe7faa47aa2a03d
Lee Chang Min <101034582+2-changmin@users.noreply.github.com>
2025-12-31 19:28:41 +0900

    Title: 중앙값 구하기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251231/중앙값 구하기/find-the-median.java
+++ 251231/중앙값 구하기/find-the-median.java
@@ -7,7 +7,7 @@ public class Main {
         int b = sc.nextInt();
         int c = sc.nextInt();
         if((a>b && b>c) || (c>b && b>a)) System.out.print(b);
-        else if((b>a && a>c) || (c>a && a>b)) System.out.print(a);
-        else if((a>c && c>b) || (b>c && c>a)) System.out.print(b);
+        if((b>a && a>c) || (c>a && a>b)) System.out.print(a);
+        if((a>c && c>b) || (b>c && c>a)) System.out.print(b);
     }
 }
